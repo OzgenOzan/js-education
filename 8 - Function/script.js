f57bdb6d437df6d7bbc48aacd5c7b07ccf5d0df0
OzgenOzan <202050014003@ogr.akdeniz.edu.tr>
2021-12-24 18:33:38 +0300

    First glance at default values in functions

--- 8 - Function/script.js
+++ 8 - Function/script.js
@@ -1,1 +1,23 @@
-'use strict';
+"use strict";
+
+const bookings = [];
+
+const createBooking = function (flightNum, numPass = 1, price = 199 * numPass) {
+  // Old way for default values
+  // numPass = numPass || 1;
+  // price = price || 199;
+
+  const booking = {
+    flightNum,
+    numPass,
+    price,
+  };
+  console.log(booking);
+
+  bookings.push(booking);
+};
+
+createBooking("Deneme123");
+createBooking("Deneme123", 3);
+createBooking("Deneme123", 5, 300);
+createBooking("Deneme123", undefined, 400);
