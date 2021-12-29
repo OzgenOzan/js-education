65007e62ad36a6f96bc94776935417d64c602d5d
OzgenOzan <202050014003@ogr.akdeniz.edu.tr>
2021-12-29 18:17:31 +0300

    finished the coding challenge 1

--- 8 - Function/script.js
+++ 8 - Function/script.js
@@ -166,8 +166,59 @@ console.log(addTax(0.1, 200));
 const addVAT = addTax.bind(null, 0.23);
 console.log(addVAT(500));
 console.log(addVAT(100));
-*/
 
 const addTax = (rate) => (value) => console.log(`${rate * value + value}`);
 
 addTax(0.18)(100);
+*/
+
+// Coding Challenge 1
+
+const poll = {
+  question: "What is your favourite programming language?",
+  options: ["0: JavaScript", "1: Python", "2: Rust", "3: C++"],
+  // This generates [0, 0, 0, 0]. More in the next section!
+  answers: new Array(4).fill(0),
+  registerNewAnswer() {
+    const answer = prompt(
+      `${this.question}\n${this.options.join("\n")}\n(Write the number)`
+    );
+    console.log(answer);
+    (typeof answer === "0" || "1" || "2" || "3") &&
+      answer < this.answers.length &&
+      this.answers[answer]++;
+
+    this.displayResults();
+    this.displayResults("string");
+  },
+
+  displayResults: function (type = "array") {
+    if (type === "array") {
+      console.log(this.answers);
+    } else if (type === "string") {
+      console.log(`Poll results are ${this.answers.join(", ")}`);
+    }
+  },
+};
+
+document
+  .querySelector(".poll")
+  .addEventListener("click", poll.registerNewAnswer.bind(poll));
+
+poll.displayResults.call({ answers: [5, 4, 3] }, "string");
+
+// poll.registerNewAnswer = function () {
+//   let input = prompt(
+//     `${this.question}\n${this.options.join("\n")}\n(Write the number)`
+//   );
+
+//   if (!isNaN(input) & (input <= 3) & (input != "") & (input != undefined)) {
+//     this.answers[Math.trunc(input)]++;
+//   } else if (Array.isArray(input)) {
+//     console.log(input);
+//   } else {
+//     console.log("Not a proper answer");
+//   }
+
+//   poll.displayResults();
+// };
