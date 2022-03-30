60194591bdb740bcdeaea062a73f59567b6b0a4f
OzgenOzan <202050014003@ogr.akdeniz.edu.tr>
2022-03-30 22:33:10 +0300

    ...

--- 9 - Working with Arrays/script.js
+++ 9 - Working with Arrays/script.js
@@ -100,7 +100,7 @@ const calcDisplaySummary = function (movements) {
     .filter((mov) => mov > 0)
     .map((deposit) => (deposit * 1.2) / 100)
     .filter((int, i, arr) => {
-      console.log(arr);
+      // console.log(arr);
       return int >= 1;
     })
     .reduce((acc, int) => acc + int, 0);
@@ -117,8 +117,8 @@ const createUsernames = function (accs) {
       .join("");
   });
 };
-
 createUsernames(accounts);
+
 // console.log(accounts);
 
 /////////////////////////////////////////////////
@@ -292,7 +292,7 @@ const calcAverageHumanAge = function (ages) {
 };
 
 calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
-*/
+
 
 const eurToUsd = 1.1;
 const totalDepositUSD = movements
@@ -301,3 +301,35 @@ const totalDepositUSD = movements
   .reduce((acc, mov) => acc + mov, 0);
 
 console.log(totalDepositUSD);
+
+// Coding challenge 3
+const calcAverageHumanAge = function (ages) {
+  const humanAges = ages
+    .map((age, i, arr) => (age <= 2 ? 2 * age : 16 + age * 4))
+    .filter((age) => age > 18)
+    .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
+  return humanAges;
+};
+
+console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]));
+console.log(calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]));
+
+
+// find method returns the first element (not array)
+
+console.log(accounts);
+
+const account = accounts.find((acc) => acc.owner === "Jessica Davis");
+console.log(account);
+
+let arr = [];
+
+for (const account of accounts) {
+  if (account.owner === "Jessica Davis") {
+    arr = account;
+    break;
+  }
+}
+
+console.log(arr);
+*/
